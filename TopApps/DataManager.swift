47a9934e4ce7740dcd6c6c9bd9b1a579c0c43f87
Svyatoslav <SvGoldKr@gmail.com>
2016-08-16 07:52:38 +0300

    test
    expect declaration

---
+++ TopApps/DataManager.swift
@@ -0,0 +1,68 @@
+//
+//  DataManager.swift
+//  TopApps
+//
+//  Created by Dani Arnaout on 9/2/14.
+//  Edited by Eric Cerney on 9/27/14.
+//  Copyright (c) 2014 Ray Wenderlich All rights reserved.
+//  Updated by SwiftBook.ru 24/11/2015
+
+import Foundation
+
+let TopAppURL = "https://itunes.apple.com/us/rss/topgrossingipadapplications/limit=25/json"
+
+class DataManager {
+  
+  class func getTopAppsDataFromFileWithSuccess(success: ((data: NSData) -> Void)) {
+    //1
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+      //2
+      let filePath = NSBundle.mainBundle().pathForResource("TopApps",ofType:"json")
+   
+      do {
+        let data = try NSData(contentsOfFile:filePath!,
+          options: NSDataReadingOptions.DataReadingUncached)
+        success(data: data)
+      } catch let error as NSError {
+        print("error: \(error.localizedDescription)")
+      } catch {
+        fatalError()
+      }
+    })
+  }
+  
+  class func loadDataFromURL(url: NSURL, completion:(data: NSData?, error: NSError?) -> Void) {
+    let session = NSURLSession.sharedSession()
+    
+    // Use NSURLSession to get data from an NSURL
+    let loadDataTask = session.dataTaskWithURL(url, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+      if let responseError = error {
+        completion(data: nil, error: responseError)
+      } else if let httpResponse = response as? NSHTTPURLResponse {
+        if httpResponse.statusCode != 200 {
+          let statusError = NSError(domain:"com.raywenderlich", code:httpResponse.statusCode, userInfo:[NSLocalizedDescriptionKey : "HTTP status code has unexpected value."])
+          completion(data: nil, error: statusError)
+        } else {
+          completion(data: data, error: nil)
+        }
+      }
+    })
+    
+    loadDataTask.resume()
+  }
+    
+    class func getTopAppsDataFromItunesWithSuccess(success: ((iTunesData: NSData!) -> Void)) {
+        //1
+        loadDataFromURL(NSURL(string: TopAppURL)!, completion:{(data, error) -> Void in
+            //2
+            if let urlData = data {
+                //3
+                success(iTunesData: urlData)
+            }
+        })
+    }
+    
+}
+
+
+
---
+++ TopApps/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TopApps
+//
+//  Created by Dani Arnaout on 9/1/14.
+//  Edited by Eric Cerney on 9/27/14.
+//  Copyright (c) 2014 Ray Wenderlich All rights reserved.
+//  Updated by SwiftBook.ru 24/11/2015
+import UIKit
+
+class ViewController: UIViewController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DataManager.getTopAppsDataFromFileWithSuccess { (data) -> Void in
+            // Get #1 app name using SwiftyJSON
+            let json = JSON(data: data)
+            if let appName = json["feed"]["entry"][0]["im:name"]["label"].string {
+                print("SwiftyJSON: \(appName)")
+            }
+        }
+    
+    }
+    
+    DataManager.getTopAppsDataFromItunesWithSuccess { (iTunesData) -> Void in
+    let json = JSON(data: iTunesData)
+    if let appName = json["feed"]["entry"][0]["im:name"]["label"].string {
+    print("NSURLSession: \(appName)")
+    }
+    // More soon...
+    }
+  }
+
+
